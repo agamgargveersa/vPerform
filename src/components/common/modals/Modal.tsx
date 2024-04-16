d1f29a868105e83c214b577f8612412655da38ba
DavikChaudharyVeersa <davik@veersatech.com>
2024-04-16 18:17:23 +0530

    Modal template created

---
+++ src/components/common/modals/Modal.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import crossImage from "../../../assets/images/cross.svg";
+import "./Modal.css";
+import { Dialog } from "@mui/material";
+
+interface ModalProps {
+  open: boolean;
+  onClose: () => void;
+  logo: string;
+  title: string;
+  content: string;
+  action: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  open,
+  onClose,
+  logo,
+  title,
+  content,
+  action,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullWidth
+      maxWidth="md"
+      sx={{
+          Width: "56.375rem",
+          Height: "43.375rem",
+          margin: "auto",
+      }}
+    >
+      {/* header section start */}
+      <div className="headerContainer">
+      <div className="headerSection">
+        <div className="leftHeaderSection">
+          <img src={logo} alt="" />
+          <p>{title}</p>
+        </div>
+        <div className="rightHeaderSection">
+          <button className="closeButton" onClick={onClose}>
+            <img src={crossImage} alt="closeImage" className="crossImage" />
+          </button>
+        </div>
+      </div>
+      </div>
+      {/* header section end */}
+      
+      {/* content section start */}
+      <div className="contentContainer">
+        <div className="contentSection">
+          {content}
+        
+        </div>
+      </div>
+      {/* content section end */}
+
+      {/* footer section start */}
+      <div className="footerContainer">
+        <div className="buttonDiv">
+          <button className="actionButton">
+            {action}
+            submit
+            </button>
+        </div>
+      </div>
+      {/* footer section end */}
+    </Dialog>
+  );
+};
+
+export default Modal;
